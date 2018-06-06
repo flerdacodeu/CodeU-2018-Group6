16bd9dab9da025738cf87971efdb1575695d4b04
argyrogounari <argyrogounari@gmail.com>
2018-06-06 23:49:26 +0100

    Update Question2.java

--- argyrogounari/assignment1/Question2.java
+++ argyrogounari/assignment1/Question2.java
@@ -1,4 +1,11 @@
-
+/**
+* Question 2: Implement an algorithm to find the kth to last 
+* element of a singly linked list.
+*
+* @author  Argyro Gounari
+* @version 1.1
+* @since   15-05-2018 
+*/
 public class Question2 {
 
 	public static void main(String[] args) {
