f3acd065b0905c8fcb49aaea2f02c0292a4d10c4
anap1003 <anap1003@gmail.com>
2018-06-19 12:01:00 +0200

    Word search implementation and test

--- anap1003/assignment3/Assignment3.java
+++ anap1003/assignment3/Assignment3.java
@@ -5,26 +5,113 @@
  */
 package assignment3;
 
+import java.util.BitSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 /**
  *
- * @author Ana
  */
 public class Assignment3 {
+    private int rows, cols;
+    private Set<String> foundWords;
+    private char[][] grid;
     
-    public Set wordSearch() {
-        Set<String> words = new LinkedHashSet();
-        
-        return words;
-    }
-
     /**
-     * @param args the command line arguments
+     * 
+     * @param gridToSearch
+     * @param dictionary
+     * @return
+     * @throws Exception 
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public Set wordSearch(char[][] gridToSearch, Dictionary dictionary) throws Exception {
+        foundWords = new LinkedHashSet();
+        
+        grid = gridToSearch;
+        rows = grid.length;
+        cols = grid[0].length;
+        BitSet visited = new BitSet(grid.length * grid[0].length);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                searchGridFromPosition(i, j, String.valueOf(grid[i][j]), visited, dictionary);
+            }
+        }
+        
+        return foundWords;
+    }
+    
+    /**
+     * 
+     * @param i
+     * @param j
+     * @param currentWord
+     * @param visited
+     * @param dictionary
+     * @throws Exception 
+     */
+    private void searchGridFromPosition(int i, int j, String currentWord, BitSet visited, Dictionary dictionary) throws Exception {
+        if (dictionary.isWord(currentWord)) {
+            foundWords.add(currentWord);
+        }
+        visited.set(i * cols + j); // recursively each attempted path sets its visited bit to true
+        
+        // attempting all possible directions of search from the current position
+        continueSearchFromPosition(i-1, j-1, visited, dictionary, currentWord);
+        
+        continueSearchFromPosition(i-1, j, visited, dictionary, currentWord);
+        
+        continueSearchFromPosition(i-1, j+1, visited, dictionary, currentWord);
+        
+        continueSearchFromPosition(i, j-1, visited, dictionary, currentWord);
+        
+        continueSearchFromPosition(i, j+1, visited, dictionary, currentWord);
+        
+        continueSearchFromPosition(i+1, j-1, visited, dictionary, currentWord);
+        
+        continueSearchFromPosition(i+1, j, visited, dictionary, currentWord);
+        
+        continueSearchFromPosition(i+1, j+1, visited, dictionary, currentWord);
+        
+        visited.set(i * cols + j, false); // once all possible paths from position (i,j) are exhausted, the bit is set to false
+    }
+    
+    /**
+     * 
+     * @param k
+     * @param t
+     * @param visited
+     * @param dictionary
+     * @param currentWord
+     * @throws Exception 
+     */
+    private void continueSearchFromPosition(int k, int t, BitSet visited, Dictionary dictionary, String currentWord) throws Exception {
+        if (isWithinBounds(k, t) && notVisited(k, t, visited)) {
+            String suffix = String.valueOf(grid[k][t]);
+            if (dictionary.isPrefix(currentWord + suffix)) {
+                searchGridFromPosition(k, t, currentWord + suffix, visited, dictionary);
+            }
+        }
+    }
+    
+    /**
+     * 
+     * @param i
+     * @param j
+     * @return 
+     */
+    private boolean isWithinBounds(int i, int j) {
+        return i >=0 && i < rows && j >= 0 && j < cols;
+    }
+    
+    /**
+     * 
+     * @param i
+     * @param j
+     * @param visited
+     * @return 
+     */
+    private boolean notVisited(int i, int j, BitSet visited) {
+        return !visited.get(i * cols + j);
     }
     
 }
--- anap1003/assignment3/DictionaryTest.java
+++ anap1003/assignment3/DictionaryTest.java
@@ -5,13 +5,9 @@
  */
 
 import assignment3.Dictionary;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
