c49fce3c3d27de4072cb227c48bfb911e136cb9d
argyrogounari <argyrogounari@gmail.com>
2018-06-22 01:44:20 +0300

    Update SinglyLinkedList.java

--- argyrogounari/assignment1/SinglyLinkedList.java
+++ argyrogounari/assignment1/SinglyLinkedList.java
@@ -9,46 +9,132 @@ public class SinglyLinkedList<T> {
 
 	private Node<T> head;
 	private Node<T> tail;
-	private int size = 0;
+	private Node<T> reversedHead = this.reverse(head);
 	
 	public SinglyLinkedList() {};
 	
+	/**
+	* Finds the kth to last 
+	* element of a singly linked list.
+	* 
+	* @param k
+	*/
 	public void findK(int k) {
 		
-		if (k>this.size-1){
-			System.out.println("You can onlu choose a k between 0 and "+ (this.size-1) + ".");
+		if (k<0 || k>=this.size()) {
+			System.out.println(k + " is a wrong number.");
 		} else {
-			Node<T> answer = this.head;
-			for(int i = this.size-1; k<i;k++){
+			
+			Node<T> answer = reversedHead;
+			for(int i=0; i<k; i++) {
 				answer = answer.getNext();
-				System.out.println("Answer: "+ answer.getData());
 			}
-			System.out.println(answer.getData());
+			if (answer==null) {
+				System.out.println(k + " is a wrong number.");
+			} else {
+				System.out.println("The "+ k + " to last element of a singly linked list is "+ answer.getData());
+			}
+			
 		}
 		
 	}
 	
 	public boolean isEmpty() {
-		return size == 0;
+		return this.head == null;
+	}
+	
+	public void removeHead() {
+		if (isEmpty()) {
+			this.head = null;
+			this.tail = null;
+		} else {
+			this.head = this.head.getNext();
+		}
+	}
+	
+	public Node<T> reverse(Node<T> head) {
+		
+		if (head == null) {
+			return head;
+		}
+		
+		Node<T> current = head;
+		Node<T> previous = null;
+		Node<T> next = null;
+		
+		while(current!=null) {
+			next = current.next;
+			current.next = previous;
+			previous = current;
+			current = next;
+		}
+		return previous;
 	}
 	
 	public void addHead(T data) {
-		this.head = new Node<>(data, this.head);
-		if(this.isEmpty()){
+		if (isEmpty()) {
+			this.head = new Node<T>( data, null);
 			this.tail = this.head;
-		} 
-		this.size++;
+		} else {
+			this.head = new Node<T>( data, this.head);
+		}
+		
 	}
 	
 	public void addTail(T data) {
-		Node<T> newNode = new Node<>(data, null);
-		if(this.isEmpty()){
-			this.head = newNode;
+		this.tail = new Node<T>( data, null);
+		if (isEmpty()) {
+			this.head = this.tail;
 		} else {
-			this.tail.setNext(newNode);
+			Node<T> newNode = new Node<T>( data, null);
+			this.tail = new Node<T>( this.tail.getData(), newNode);
+			this.tail = newNode;
 		}
-		this.tail = newNode;
-		this.size++;
+	}
+	
+	public Node<T> getHead() {
+		if(isEmpty()) {
+			return null;
+		}
+		return this.head;
+	}
+	
+	public Node<T> getReversedHead() {
+		if(isEmpty()) {
+			return null;
+		}
+		return this.reversedHead;
+	}
+	
+	public T getTail() {
+		if(isEmpty()){
+			return null;
+		}
+		return this.tail.getData();
+	}
+	
+	public void display(Node<T> head) {
+		if(head == null){
+			return;
+		}
+		Node<T> current = head;
+		while(current!=null) {
+			System.out.print(current.getData() + " --> ");
+			current = current.next;
+		}
+		System.out.println(current);
+	}
+	
+	private int size() {
+		int counter = 0;
+		Node<T> current = head;
+		while(true) {
+			if (current == null) {
+				return counter;
+			}
+			counter++;
+			current = current.getNext();
+		}
 	}
 	
 }
