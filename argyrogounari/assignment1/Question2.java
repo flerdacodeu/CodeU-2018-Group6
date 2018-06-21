ecb4a0a8ee45dee1ae66722122558a53e9986322
argyrogounari <argyrogounari@gmail.com>
2018-06-22 01:42:03 +0300

    Update Question2.java

--- argyrogounari/assignment1/Question2.java
+++ argyrogounari/assignment1/Question2.java
@@ -11,12 +11,17 @@ public class Question2 {
 	public static void main(String[] args) {
 		
 		SinglyLinkedList<Integer> newList = new SinglyLinkedList<>();
-		newList.addHead(4);
+		
 		newList.addHead(3);
 		newList.addHead(2);
 		newList.addHead(1);
 		newList.addHead(0);
-		newList.findK(4);
+		newList.display(newList.getHead());
+		
+		newList.findK(-1); // Error: Wrong number message
+		newList.findK(0);  // Error: Wrong number message
+		newList.findK(4);  // Error: Wrong number message
+		newList.findK(5);  // Error: Wrong number message
 		
 	}
 
