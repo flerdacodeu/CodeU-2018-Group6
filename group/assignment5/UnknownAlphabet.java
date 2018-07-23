7f663ae8ac94477e7282f6f4064d5810713464b1
anap1003 <anap1003@gmail.com>
2018-07-23 18:45:04 +0200

    Updated group's solution
    Added solution to the optional challenge, changed some variables names to camelCase, added tests for optional solution

--- group/assignment5/UnknownAlphabet.java
+++ group/assignment5/UnknownAlphabet.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
@@ -11,33 +12,46 @@ import java.util.Set;
 // white = not yet visited; grey = visited, but not yet finished; black = finished.
 class Vertex {
 
-	final char value;
-	Set<Vertex> children = new HashSet<Vertex>();
-	String state = "white";
+    final Character value;
+    Set<Vertex> children = new HashSet<Vertex>();
+    String state = "white";
+    int depth = 0;
 
-	Vertex(char value) {
-		this.value = value;
-	}
+    Vertex(Character value) {
+        this.value = value;
+    }
 
-	public void addChild(Vertex child) {
-		children.add(child);
-	}
+    public void addChild(Vertex child) {
+        children.add(child);
+    }
 
-	public void setState(String state) {
-		this.state = state;
-	}
+    public void setState(String state) {
+        this.state = state;
+    }
 
-	public char getValue() {
-		return value;
-	}
+    public Character getValue() {
+        return value;
+    }
 
-	public Set<Vertex> getChildren() {
-		return children;
-	}
+    public Set<Vertex> getChildren() {
+        return children;
+    }
 
-	public String getState() {
-		return state;
-	}
+    public String getState() {
+        return state;
+    }
+    
+    public int getDepth() {
+        return depth;
+    }
+    
+    public void increaseDepth() {
+        depth++;
+    }
+    
+    public void decreaseDepth() {
+        depth--;
+    }
 
 }
 
@@ -47,148 +61,206 @@ class Vertex {
 // Keep a map of every char to its vertex to check if a vertex was already created for that char.
 public class UnknownAlphabet {
 
-	LinkedList<Character> alphabet = new LinkedList<Character>();
-	LinkedList<Vertex> vertices = new LinkedList<Vertex>();
-	HashMap<Character, Vertex> char_vertex_map = new HashMap<Character, Vertex>();
-	
-	// Assume dictionary input is a List.
-	public LinkedList<Character> getAlphabet(List<String> dict) throws Exception {
+    LinkedList<Character> alphabet = new LinkedList<Character>();
+    LinkedList<Character> currentAlphabet = new LinkedList<Character>();
+    LinkedList<LinkedList<Character>> allPossibleAlphabets = new LinkedList<>();
+    LinkedList<Vertex> vertices = new LinkedList<Vertex>();
+    HashMap<Character, Vertex> mapCharToVertex = new HashMap<Character, Vertex>();
 
-		if (dict.isEmpty())
-			return new LinkedList<Character>();
+    // Assume dictionary input is a List.
+    public LinkedList<Character> getAlphabet(List<String> dict) throws Exception {
 
-		// Return all characters (no duplicates) if dictionary has only one word.
-		if (dict.size() == 1) 
-			alphabetForOneWord(dict.get(0));
-		
-		else{
-			createVertices(dict);
-			topologicalSort();
-		}
+        if (dict.isEmpty()) {
+            return new LinkedList<Character>();
+        }
 
-		return alphabet;
-	}
-	
-	private void alphabetForOneWord(String word){
-		
-		for (int i = 0; i < word.length(); i++) {
-			Character ch = word.charAt(i);
-			if(!alphabet.contains(ch))
-				alphabet.add(ch);
-		}
-	}
+        // Return all characters (no duplicates) if dictionary has only one word.
+        if (dict.size() == 1) {
+            alphabetForOneWord(dict.get(0));
+        } else {
+            createVertices(dict);
+            topologicalSort();
+        }
 
-	// Traverse verticies which have not been visited yet.
-	private void topologicalSort() throws Exception {
+        return alphabet;
+    }
+    
+    public LinkedList<LinkedList<Character>> getAllAlphabets(List<String> dict) throws Exception {
+        if (dict.isEmpty()) {
+            return new LinkedList<>();
+        }
 
-		for (Vertex vertex : vertices) {
-			if (vertex.getState().equals("white")) {
-				sortFromVertex(vertex);
-			}
-		}
-	}
+        if (dict.size() == 1) {
+            alphabetForOneWord(dict.get(0));
+        } else {
+            createVertices(dict);
+            allTopologicalSort();
+        }
 
-	// For each unvisisted vertex, visit its children if they haven't been visited (white).
-	// If during the visit of a child we discover it was started but not finished (grey),
-	// then we have a cycle, so no order is possible.
-	private void sortFromVertex(Vertex cur_vertex) throws Exception {
+        return allPossibleAlphabets;
+    }
 
-		cur_vertex.setState("grey"); // starting visit, but not finished
-		for (Vertex child : cur_vertex.getChildren()) {
-			if (child.getState().equals("white")) {
-				sortFromVertex(child);
-			} else if (child.getState().equals("grey")) {
-				throw new Exception("There is a cycle, order impossible.");
-			}
-		}
-		cur_vertex.setState("black"); // finished visit.
-		alphabet.addFirst(cur_vertex.getValue());
-	}
+    private void alphabetForOneWord(String word) {
 
-	// Look at every 2 consecutive words to get the relation between the word_first 2 different characters.
-	// For example, in [ABCD, ABDE, EFGH, FMNO]: C < D (from word_first 2 words), A < E (from 2nd and 3rd), E < F (from 3rd and 4th)
-	// Because A < E and E < F then from transitivity => A < E without needing to check word 1 or 2 with word 4.
-	public void createVertices(List<String> dict) {
+        for (int i = 0; i < word.length(); i++) {
+            Character ch = word.charAt(i);
+            if (!alphabet.contains(ch)) {
+                alphabet.add(ch);
+            }
+        }
+    }
 
-		boolean isSecondWordLast = false;
-		int dictSize = dict.size();
-		
-		for (int i = 0; i < dictSize - 1; i++) {
-			String word_first = dict.get(i);
-			String word_second = dict.get(i + 1);
-			
-			if(i == dictSize - 1)
-				isSecondWordLast = true; 
-			
-			createVerticesHelper(word_first, word_second, isSecondWordLast);
-		}
-	}
+    // Traverse verticies which have not been visited yet.
+    private void topologicalSort() throws Exception {
 
-	private void createVerticesHelper(String word_first, String word_second, boolean isSecondWordLast) {
+        for (Vertex vertex : vertices) {
+            if (vertex.getState().equals("white")) {
+                sortFromVertex(vertex);
+            }
+        }
+    }
 
-		int length_first = word_first.length();
-		int length_second = word_second.length();
-		int min_length = Math.min(length_first, length_second);
+    private void allTopologicalSort() throws Exception {
+        boolean allFound = false;
 
-		int index = 0;
+        for (Vertex vertex : vertices) {
+            if (vertex.getState().equals("white") && vertex.getDepth() == 0) {
+                vertex.setState("black");
+                // "delete" the branch to every child by decreasing the depth
+                Iterator<Vertex> iter = vertex.getChildren().iterator();
+                while (iter.hasNext()) {
+                    Vertex child = iter.next();
+                    child.decreaseDepth();
+                }
 
-		// Iterate through word_first and word_second word until they have a different character.
-		// For each new character, create its vertex and add it to the list using createVertex.
-		while (index < min_length) {
-			char char_first = word_first.charAt(index);
-			char char_second = word_second.charAt(index);
+                currentAlphabet.add(vertex.getValue());
+                allTopologicalSort();
 
-			if (char_first == char_second) {
-				createVertex(char_first);
-			}
+                // reset the vertix info allowing it to be chosen again in another combination
+                vertex.setState("white");
+                iter = vertex.getChildren().iterator();
+                while (iter.hasNext()) {
+                    Vertex child = iter.next();
+                    child.increaseDepth();
+                }
+                currentAlphabet.remove(vertex.getValue());
 
-			else {
-				Vertex vertex_first = createVertex(char_first);
-				Vertex vertex_second = createVertex(char_second);
-				vertex_first.addChild(vertex_second);
-				break;
-			}
+                allFound = true;
+            }
+        }
 
-			index++;
-		}
+        if (!allFound) {
+            // make different lists for each possible alphabet
+            LinkedList<Character> newAlphabet = new LinkedList<>();
+            for (Character letter : currentAlphabet) {
+                newAlphabet.add(letter);
+            }
+            allPossibleAlphabets.add(newAlphabet);
+        }
 
-		int index_second = index;
-		
-		// Create vertices for characters until the end of word_first  word.
-		while (index < length_first) {
-			createVertex(word_first.charAt(index));
-			index++;
-		}
+    }
 
-		if (isSecondWordLast) {
-			while (index_second < length_second) {
-				createVertex(word_second.charAt(index_second));
-				index_second++;
-			}
-		}
+    // For each unvisisted vertex, visit its children if they haven't been visited (white).
+    // If during the visit of a child we discover it was started but not finished (grey),
+    // then we have a cycle, so no order is possible.
+    private void sortFromVertex(Vertex currentVertex) throws Exception {
 
-	}
-			
-	// Auxiliary method, keep a map of already created vertices for given chars.
-	// Only create new vertex and update map and list if char not seen before (not in map).
-	private Vertex createVertex(char ch) {
-		
-		Vertex vertex;
+        currentVertex.setState("grey"); // starting visit, but not finished
+        for (Vertex child : currentVertex.getChildren()) {
+            if (child.getState().equals("white")) {
+                sortFromVertex(child);
+            } else if (child.getState().equals("grey")) {
+                throw new Exception("There is a cycle, order impossible.");
+            }
+        }
+        currentVertex.setState("black"); // finished visit.
+        alphabet.addFirst(currentVertex.getValue());
+    }
 
-		if (!char_vertex_map.containsKey(ch)) {
-			vertex = new Vertex(ch);
-			char_vertex_map.put(ch, vertex);
-			vertices.add(vertex);
-		} else {
-			vertex = char_vertex_map.get(ch);
-		}
+    // Look at every 2 consecutive words to get the relation between the word_first 2 different characters.
+    // For example, in [ABCD, ABDE, EFGH, FMNO]: C < D (from word_first 2 words), A < E (from 2nd and 3rd), E < F (from 3rd and 4th)
+    // Because A < E and E < F then from transitivity => A < E without needing to check word 1 or 2 with word 4.
+    public void createVertices(List<String> dict) {
 
-		return vertex;
-	}
-	
-	// Getter method for testing.
-	public LinkedList<Vertex> getListOfVertices(){
-		return vertices;
-	}
+        boolean isSecondWordLast = false;
+        int dictSize = dict.size();
+
+        for (int i = 0; i < dictSize - 1; i++) {
+            String firstWord = dict.get(i);
+            String secondWord = dict.get(i + 1);
+
+            if (i == dictSize - 1) {
+                isSecondWordLast = true;
+            }
+
+            createVerticesHelper(firstWord, secondWord, isSecondWordLast);
+        }
+    }
+
+    private void createVerticesHelper(String firstWord, String secondWord, boolean isSecondWordLast) {
+
+        int firstWordLength = firstWord.length();
+        int secondWordLength = secondWord.length();
+        int minLength = Math.min(firstWordLength, secondWordLength);
+
+        int index = 0;
+
+        // Iterate through word_first and word_second word until they have a different character.
+        // For each new character, create its vertex and add it to the list using createVertex.
+        while (index < minLength) {
+            char firstChar = firstWord.charAt(index);
+            char secondChar = secondWord.charAt(index);
+
+            if (firstChar == secondChar) {
+                createVertex(firstChar);
+            } else {
+                Vertex parentVertex = createVertex(firstChar);
+                Vertex childVertex = createVertex(secondChar);
+                parentVertex.addChild(childVertex);
+                childVertex.increaseDepth();
+                break;
+            }
+
+            index++;
+        }
+
+        int indexForSecondWord = index;
+
+        // Create vertices for characters until the end of word_first  word.
+        while (index < firstWordLength) {
+            createVertex(firstWord.charAt(index));
+            index++;
+        }
+
+        if (isSecondWordLast) {
+            while (indexForSecondWord < secondWordLength) {
+                createVertex(secondWord.charAt(indexForSecondWord));
+                indexForSecondWord++;
+            }
+        }
+
+    }
+
+    // Auxiliary method, keep a map of already created vertices for given chars.
+    // Only create new vertex and update map and list if char not seen before (not in map).
+    private Vertex createVertex(char ch) {
+
+        Vertex vertex;
+
+        if (!mapCharToVertex.containsKey(ch)) {
+            vertex = new Vertex(ch);
+            mapCharToVertex.put(ch, vertex);
+            vertices.add(vertex);
+        } else {
+            vertex = mapCharToVertex.get(ch);
+        }
+
+        return vertex;
+    }
+
+    // Getter method for testing.
+    public LinkedList<Vertex> getListOfVertices() {
+        return vertices;
+    }
 
 }
--- group/assignment5/UnknownAlphabetTest.java
+++ group/assignment5/UnknownAlphabetTest.java
@@ -1,3 +1,4 @@
+
 import static org.junit.Assert.*;
 
 import java.util.LinkedList;
@@ -6,89 +7,140 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
-
 public class UnknownAlphabetTest {
-	
-	@Test
-	public void testCreateVertices() throws Exception{
-		UnknownAlphabet alphabet = new UnknownAlphabet();
-		alphabet.createVertices(Arrays.asList("ART", "RAT", "CAT", "CAR"));
-		LinkedList<Vertex> vertices = alphabet.getListOfVertices();
-		
-		assertEquals(vertices.size(), 4);
-		
-		assertEquals(vertices.get(0).getValue(), 'A');
-		assertEquals(vertices.get(0).getChildren().size(), 1);
-		
-		assertEquals(vertices.get(1).getValue(), 'R');
-		assertEquals(vertices.get(1).getChildren().size(), 1);
-		
-		assertEquals(vertices.get(2).getValue(), 'T');
-		assertEquals(vertices.get(2).getChildren().size(), 1);
-		
-		assertEquals(vertices.get(3).getValue(), 'C');
-		assertEquals(vertices.get(3).getChildren().size(), 0);		
-	}
-	
-	@Test(expected = Exception.class)
-	public void testCycleSmallExample() throws Exception{
-		UnknownAlphabet alphabet = new UnknownAlphabet();
-		alphabet.getAlphabet(Arrays.asList("abc","acd","abd"));
-	}
-	
-	@Test(expected = Exception.class)
-	public void testCycleBiggerExample() throws Exception{
-		UnknownAlphabet alphabet = new UnknownAlphabet();
-		// ... calm < ... < cute < cat ...
-		alphabet.getAlphabet(Arrays.asList("bear","better","calm","clear",
-				"club","cluster", "cute", "cat", "ready","rear"));
-	}
-	
-	
-	@Test
-	public void testNoWord() throws Exception {
-		UnknownAlphabet alphabet = new UnknownAlphabet();
-		assertEquals(alphabet.getAlphabet(new ArrayList<String>()).size(),0);
-	}
-	
-	@Test
-	public void testOneWord() throws Exception {
-		UnknownAlphabet alphabet = new UnknownAlphabet();
-		LinkedList<Character> correctResult = new LinkedList<Character>(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F','G'));
-		assertEquals(alphabet.getAlphabet(Arrays.asList("ABCDEFG")), correctResult);
-		assertEquals(alphabet.getAlphabet(Arrays.asList("ABACDBEFEGGG")), correctResult);
-	}
 
-	@Test
-	public void testSmallExample() throws Exception {
-		UnknownAlphabet alphabet = new UnknownAlphabet();
-		LinkedList<Character> result = alphabet.getAlphabet(Arrays.asList("ART", "RAT", "CAT", "CAR"));
-		
-		System.out.println(result.toString()); // [T, A, R, C]
+    @Test
+    public void testCreateVertices() throws Exception {
+        UnknownAlphabet alphabet = new UnknownAlphabet();
+        alphabet.createVertices(Arrays.asList("ART", "RAT", "CAT", "CAR"));
+        LinkedList<Vertex> vertices = alphabet.getListOfVertices();
 
-		assertTrue(result.indexOf('A') < result.indexOf('R'));
-		assertTrue(result.indexOf('R') < result.indexOf('C'));
-		assertTrue(result.indexOf('T') < result.indexOf('R'));
-	}
-	
-	@Test
-	public void testBiggerExample() throws Exception {
-		UnknownAlphabet alphabet = new UnknownAlphabet();
-		LinkedList<Character> result = alphabet.getAlphabet(Arrays.asList("bear","better","calm","clear",
-				"club","cluster", "cute", "ready","rear"));
-		
-		System.out.println(result.toString()); // [y, d, m, a, t, l, e, u, b, s, c, r]
-		
-		assertTrue(result.indexOf('a') < result.indexOf('t')); // beAr < beTter
-		assertTrue(result.indexOf('b') < result.indexOf('c')); // Better < Calm
-		assertTrue(result.indexOf('a') < result.indexOf('l')); // cAlm < cLear
-		assertTrue(result.indexOf('e') < result.indexOf('u')); // clEar < clUb
-		assertTrue(result.indexOf('b') < result.indexOf('s')); // cluB < cluSter
-		assertTrue(result.indexOf('l') < result.indexOf('u')); // cLuster < cUter
-		assertTrue(result.indexOf('a') < result.indexOf('u')); // cAlm < ... < cUte
-		assertTrue(result.indexOf('c') < result.indexOf('r')); // Cute < Ready
-		assertTrue(result.indexOf('b') < result.indexOf('r')); // Better < .. < Ready
-		assertTrue(result.indexOf('d') < result.indexOf('r')); // reaDy < reaR
-	}
-	
+        assertEquals(vertices.size(), 4);
+
+        // need to add explicit boxing, otherwise an ambiguous call
+        assertEquals(vertices.get(0).getValue(), (Character) 'A');
+        assertEquals(vertices.get(0).getChildren().size(), 1);
+
+        assertEquals(vertices.get(1).getValue(), (Character) 'R');
+        assertEquals(vertices.get(1).getChildren().size(), 1);
+
+        assertEquals(vertices.get(2).getValue(), (Character) 'T');
+        assertEquals(vertices.get(2).getChildren().size(), 1);
+
+        assertEquals(vertices.get(3).getValue(), (Character) 'C');
+        assertEquals(vertices.get(3).getChildren().size(), 0);
+    }
+
+    @Test(expected = Exception.class)
+    public void testCycleSmallExample() throws Exception {
+        UnknownAlphabet alphabet = new UnknownAlphabet();
+        alphabet.getAlphabet(Arrays.asList("abc", "acd", "abd"));
+    }
+
+    @Test(expected = Exception.class)
+    public void testCycleBiggerExample() throws Exception {
+        UnknownAlphabet alphabet = new UnknownAlphabet();
+        // ... calm < ... < cute < cat ...
+        alphabet.getAlphabet(Arrays.asList("bear", "better", "calm", "clear",
+                "club", "cluster", "cute", "cat", "ready", "rear"));
+    }
+
+    @Test
+    public void testNoWord() throws Exception {
+        UnknownAlphabet alphabet = new UnknownAlphabet();
+        assertEquals(alphabet.getAlphabet(new ArrayList<String>()).size(), 0);
+    }
+
+    @Test
+    public void testOneWord() throws Exception {
+        UnknownAlphabet alphabet = new UnknownAlphabet();
+        LinkedList<Character> correctResult = new LinkedList<Character>(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G'));
+        assertEquals(alphabet.getAlphabet(Arrays.asList("ABCDEFG")), correctResult);
+        assertEquals(alphabet.getAlphabet(Arrays.asList("ABACDBEFEGGG")), correctResult);
+    }
+
+    @Test
+    public void testSmallExample() throws Exception {
+        UnknownAlphabet alphabet = new UnknownAlphabet();
+        LinkedList<Character> result = alphabet.getAlphabet(Arrays.asList("ART", "RAT", "CAT", "CAR"));
+
+        System.out.println(result.toString()); // [T, A, R, C]
+
+        assertTrue(result.indexOf('A') < result.indexOf('R'));
+        assertTrue(result.indexOf('R') < result.indexOf('C'));
+        assertTrue(result.indexOf('T') < result.indexOf('R'));
+    }
+
+    @Test
+    public void testBiggerExample() throws Exception {
+        UnknownAlphabet alphabet = new UnknownAlphabet();
+        LinkedList<Character> result = alphabet.getAlphabet(Arrays.asList("bear", "better", "calm", "clear",
+                "club", "cluster", "cute", "ready", "rear"));
+
+        System.out.println(result.toString()); // [y, d, m, a, t, l, e, u, b, s, c, r]
+
+        assertTrue(result.indexOf('a') < result.indexOf('t')); // beAr < beTter
+        assertTrue(result.indexOf('b') < result.indexOf('c')); // Better < Calm
+        assertTrue(result.indexOf('a') < result.indexOf('l')); // cAlm < cLear
+        assertTrue(result.indexOf('e') < result.indexOf('u')); // clEar < clUb
+        assertTrue(result.indexOf('b') < result.indexOf('s')); // cluB < cluSter
+        assertTrue(result.indexOf('l') < result.indexOf('u')); // cLuster < cUter
+        assertTrue(result.indexOf('a') < result.indexOf('u')); // cAlm < ... < cUte
+        assertTrue(result.indexOf('c') < result.indexOf('r')); // Cute < Ready
+        assertTrue(result.indexOf('b') < result.indexOf('r')); // Better < .. < Ready
+        assertTrue(result.indexOf('d') < result.indexOf('r')); // reaDy < reaR
+    }
+
+    @Test
+    public void onePossibleAlphabetTest() throws Exception {
+        UnknownAlphabet alphabet = new UnknownAlphabet();
+        LinkedList<LinkedList<Character>> allPossibleAlphabets = alphabet.getAllAlphabets(Arrays.asList("ART", "RAT", "CAT", "CAR", "CTA"));
+        assertEquals(1, allPossibleAlphabets.size());
+        LinkedList<Character> onlyAlphabet = allPossibleAlphabets.get(0);
+        assertTrue(onlyAlphabet.indexOf('A') < onlyAlphabet.indexOf('R'));
+        assertTrue(onlyAlphabet.indexOf('A') < onlyAlphabet.indexOf('T'));
+        assertTrue(onlyAlphabet.indexOf('R') < onlyAlphabet.indexOf('C'));
+        assertTrue(onlyAlphabet.indexOf('T') < onlyAlphabet.indexOf('R'));
+
+    }
+
+    @Test
+    public void twoPossibleAlphabetsTest() throws Exception {
+        UnknownAlphabet alphabet = new UnknownAlphabet();
+        LinkedList<LinkedList<Character>> allPossibleAlphabets = alphabet.getAllAlphabets(Arrays.asList("ART", "RAT", "CAT", "CAR"));
+        assertEquals(2, allPossibleAlphabets.size());
+        for (int i = 0; i < allPossibleAlphabets.size(); i++) {
+            LinkedList<Character> currentAlphabet = allPossibleAlphabets.get(i);
+            assertTrue(currentAlphabet.indexOf('A') < currentAlphabet.indexOf('R'));
+            assertTrue(currentAlphabet.indexOf('R') < currentAlphabet.indexOf('C'));
+            assertTrue(currentAlphabet.indexOf('T') < currentAlphabet.indexOf('R'));
+        }
+    }
+
+    @Test
+    public void manyPossibleAlphabetsTest() throws Exception {
+        UnknownAlphabet alphabet = new UnknownAlphabet();
+        LinkedList<LinkedList<Character>> allPossibleAlphabets = alphabet.getAllAlphabets(Arrays.asList("54", "50", "24", "21", "35", "30", "15"));
+        assertEquals(13, allPossibleAlphabets.size());
+        for (int i = 0; i < allPossibleAlphabets.size(); i++) {
+            LinkedList<Character> currentAlphabet = allPossibleAlphabets.get(i);
+            assertTrue(currentAlphabet.indexOf('4') < currentAlphabet.indexOf('0'));
+            assertTrue(currentAlphabet.indexOf('5') < currentAlphabet.indexOf('2'));
+            assertTrue(currentAlphabet.indexOf('4') < currentAlphabet.indexOf('1'));
+            assertTrue(currentAlphabet.indexOf('2') < currentAlphabet.indexOf('3'));
+            assertTrue(currentAlphabet.indexOf('5') < currentAlphabet.indexOf('0'));
+            assertTrue(currentAlphabet.indexOf('3') < currentAlphabet.indexOf('1'));
+        }
+    }
+
+    @Test // long test, takes between 11s to 14s for me, but finds 1.782.660 possible alphabets 
+    public void aWholeLotOfPossibleAlphabetsTest() throws Exception {
+        UnknownAlphabet alphabet = new UnknownAlphabet();
+        long millisStart = System.currentTimeMillis();
+        LinkedList<LinkedList<Character>> allPossibleAlphabets = alphabet.getAllAlphabets(Arrays.asList("BEAR", "BETTER", "CALM", "CLEAR", "CLUB", "CLUE", "CLUSTER", "CUTE", "READY", "REAR"));
+        long millisEnd = System.currentTimeMillis();
+        assertTrue(allPossibleAlphabets.size() > 1000000);
+        assertTrue((millisEnd - millisStart) / 1000 < 15);
+    }
+
 }
