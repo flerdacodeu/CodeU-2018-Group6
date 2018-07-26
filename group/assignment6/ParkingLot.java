1a7b5101fd98e7e830e5020cf50699f820b1327d
argyrogounari <argyrogounari@gmail.com>
2018-07-27 00:44:49 +0300

    Updated solution
    An updated solution with:
    “- initial check if initialFreeSpace == finalFreeSpace (so final state is a permutation of initial state) then firstly do a dummy move of a car that isn’t in its final space to initialFreeSpace. ”

--- group/assignment6/ParkingLot.java
+++ group/assignment6/ParkingLot.java
@@ -1,5 +1,3 @@
-package assignment6;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,11 +5,12 @@ import static org.junit.Assert.*;
 
 
 /**
-There is a parking lot with N spaces and N-1 cars in it. Your task is to write an algorithm torearrange the cars in a given way. Only one car can be moved at a time to the empty slot
+There is a parking lot with N spaces and N-1 cars in it. Your task is to write an algorithm 
+to rearrange the cars in a given way. Only one car can be moved at a time to the empty slot
 */
 public class ParkingLot {
     /**
-    We have assummed car have ids from 0 to N-2 and parking lots have ids from 0 to N-1. (both inclusive).
+    We have assummed cars have ids from 0 to N-2 and parking lots have ids from 0 to N-1. (both inclusive).
     We represent the parking lot state as an array, where the content of each position p is the id of the car
     situated on parking lot position p. An id of -1 represents an empty parking lot.
     */
@@ -26,6 +25,29 @@ public class ParkingLot {
         carToPosition = new int[N - 1];
         this.parkingLotCurrent = parkingLotCurrent;
         moves = new ArrayList<>();
+        if (initialEqualsFinalFreeSpace(parkingLotCurrent, parkingLotEnd)) {
+        	for (int i = 0; i < N-1; i++) {
+                if (parkingLotCurrent[i] == -1) {
+                    if (i == 0) {
+                    	int counter = i+1;
+                    	while (counter<N-1 && parkingLotCurrent[counter] == parkingLotEnd[counter])
+                    	{counter++;}
+                    	if (counter!=N-1) {
+                    		int temp = parkingLotCurrent[i];
+                    		parkingLotCurrent[i] = parkingLotCurrent[counter];
+                    		parkingLotCurrent[counter] = temp;
+                    	}
+                    } else {
+                    	int counter = i-1;
+                    	while (counter>0 && parkingLotCurrent[counter] == parkingLotEnd[counter])
+                    	{counter--;}
+                    	int temp = parkingLotCurrent[i];
+                    	parkingLotCurrent[i] = parkingLotCurrent[i-1];
+                    	parkingLotCurrent[i-1] = temp;
+                    }
+                }
+            }
+        }
         for (int i = 0; i < N; i++) {
 
             if (parkingLotCurrent[i] == -1) {
@@ -82,6 +104,25 @@ public class ParkingLot {
             assertFalse("Duplicate car found, invalid input.", found[element + 1]);
             found[element + 1] = true;
         }
+        int count = 0;
+        for (int i = 0; i < end.length; i++) {
+            if (start[i] == end[i]) {
+            	count++;
+            }
+        }
+        if (count==end.length-1) {
+        	assertFalse("No moves were needed.",found[count + 1]);
+        }
+    }
+    
+	public boolean initialEqualsFinalFreeSpace(int[] start, int[] end) {
+    	for (int i = 0; i < start.length; i++) {
+
+            if (start[i] == -1 && end[i] == -1) {
+                return true;
+            }
+        }
+    	return false;
     }
 
     public List<String> getMoves() {
