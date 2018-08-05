905ea19ef4c3915967504c857f2cd2e6ced208b2
Diana Tanase <dianaatanase95@gmail.com>
2018-08-05 15:56:54 +0100

    Updated Move
    Added new constructor for printing and testing.

--- group/assignment6/Move.java
+++ group/assignment6/Move.java
@@ -16,6 +16,16 @@ public class Move {
 		
 	}
 	
+	Move(int carId, int fromSpace, int toSpace, int[] state) {
+		this.carId = carId;
+		this.fromSpace = fromSpace;
+		this.toSpace = toSpace;
+		this.state = state[0]+"";
+		for(int i = 1; i < state.length; i++){
+			this.state = this.state + ", " + state[i];
+		}
+	}
+	
 	Move(int carId, int fromSpace, int toSpace, HashMap<Integer, Integer> state) {
 		this.carId = carId;
 		this.fromSpace = fromSpace;
